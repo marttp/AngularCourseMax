316621c68d3754400bdc717e053f661c48b3f454
Thanaphoom Babparn <mart-tp@hotmail.com>
2018-11-23 20:33:35 +0700

    Update recipe detail and list

--- shopping-mall/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-mall/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Recipe from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs2xRpcEu5BqObDL3tWA8C0bYcoq7u9Z_LgNUEPTjT5NBX51V-'
+    )
+  ];
 
   constructor() { }
 
