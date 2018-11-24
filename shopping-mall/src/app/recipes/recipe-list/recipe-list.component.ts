035e308b4ef0203bf23a1374f92a1598e1aba54e
Thanaphoom Babparn <mart-tp@hotmail.com>
2018-11-24 10:23:35 +0700

    Update shopping list and detail

--- shopping-mall/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-mall/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'A Test Recipe',
       'This is simply a test',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs2xRpcEu5BqObDL3tWA8C0bYcoq7u9Z_LgNUEPTjT5NBX51V-'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs2xRpcEu5BqObDL3tWA8C0bYcoq7u9Z_LgNUEPTjT5NBX51V-'
     )
   ];
 
