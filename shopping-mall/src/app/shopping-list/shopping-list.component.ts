5118738ee8b6d67626ccc1770f58069e620906eb
Thanaphoom.b <tp.babparn@gmail.com>
2019-05-14 14:13:48 +0700

    Update course project and other side solutions.

--- shopping-mall/src/app/shopping-list/shopping-list.component.ts
+++ shopping-mall/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import Ingredient from '../shared/ingredient.model';
+
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,10 +8,9 @@ import Ingredient from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
   ];
 
   constructor() { }
@@ -18,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
